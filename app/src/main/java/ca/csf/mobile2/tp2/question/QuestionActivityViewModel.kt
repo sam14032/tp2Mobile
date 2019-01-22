43137cffd136d740349e386eee45658ce87c031c
Jean-Raphael Auclair-Soucy <Arganancer@gmail.com>
2019-01-22 13:48:55 -0500

    Started structuring the layout and activity for annotations and databinding

---
+++ app/src/main/java/ca/csf/mobile2/tp2/question/QuestionActivityViewModel.kt
@@ -0,0 +1,16 @@
+package ca.csf.mobile2.tp2.question
+
+import android.databinding.BaseObservable
+import org.parceler.Parcel
+import org.parceler.ParcelConstructor
+
+@Parcel(Parcel.Serialization.BEAN)
+class QuestionActivityViewModel @ParcelConstructor constructor() : BaseObservable() {
+    fun onResume() {
+
+    }
+
+    fun onPause() {
+
+    }
+}
--- app/src/main/java/ca/csf/mobile2/tp2/question/QuestionService.kt
+++ app/src/main/java/ca/csf/mobile2/tp2/question/QuestionService.kt
@@ -1,13 +1,13 @@
 package ca.csf.mobile2.tp2.question
-
 import org.androidannotations.annotations.EBean
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 import retrofit2.http.GET
 
 private const val QUESTION_URL = "https://m2t2.csfpwmjv.tk"
+
 @EBean(scope = EBean.Scope.Singleton)
-class QuestionService(){
+class QuestionService{
 
     private val service : Service
 
